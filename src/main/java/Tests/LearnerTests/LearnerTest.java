a4f5f81e306132f806c241a711ef93ae69ed281d
Jared <jaredwads@gmail.com>
2015-10-22 16:52:51 -0600

    Added new Tests

--- src/main/java/Tests/LearnerTests/LearnerTest.java
+++ src/main/java/Tests/LearnerTests/LearnerTest.java
@@ -2,8 +2,10 @@ package Tests.LearnerTests;
 
 import Learner.StopCondition;
 import Learner.Learner;
+import Learner.ResultSet;
 import Oracle.Center;
 import Oracle.Passive;
+import Oracle.ReverseCenter;
 import be.abeel.util.Pair;
 import net.sf.javaml.classification.Classifier;
 import net.sf.javaml.classification.KNearestNeighbors;
@@ -25,7 +27,133 @@ import java.util.Map;
 public class LearnerTest {
 	public static void main(String[] argc) {
 		try {
-			Dataset data = ARFFHandler.loadARFF(new File("data/diabetes.arff"), 8);
+			String dataFile = "data/credit-g.arff";
+			int classLoc = 20;
+			int numItter = 10;
+
+//			runBoth   (dataFile, classLoc, numItter);
+			runPassive(dataFile, classLoc, numItter);
+			runActive (dataFile, classLoc, numItter);
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void runActive (String fileLocation, int classLoc, int pNumItter) throws FileNotFoundException {
+		double averageAccuracyActive  = 0;
+		double averageNumTestsActive  = 0;
+
+		double numItter = pNumItter;
+
+		while(pNumItter > 0) {
+			Dataset data = ARFFHandler.loadARFF(new File(fileLocation), classLoc);
+
+			NormalizeMidrange nmr = new NormalizeMidrange(0, 2);
+			nmr.build(data);
+			nmr.filter(data);
+
+			Sampling s = Sampling.SubSampling;
+
+			Pair<Dataset, Dataset> data2 = s.sample(data, (int) (data.size() * 0.7), 1);
+
+			data = data2.x();
+			Dataset testData = data2.y();
+
+			StopCondition st1 = new StopCondition(data.size());
+			StopCondition st2 = new StopCondition(10, data.size(), .72);
+
+			ReverseCenter rc = new ReverseCenter(data);
+
+			Classifier knn = new KNearestNeighbors(5);
+
+			Learner l1 = new Learner(rc, knn, testData, st2);
+
+			l1.run();
+			ResultSet rs1 = l1.getResultSet();
+
+//			System.out.println("Completed test #" + (numItter - pNumItter));
+//			System.out.println("Active Results");
+//			System.out.println("Accuracy: " + rs1.getAccuracy());
+//			System.out.println("NumTests: " + rs1.getNumTests());
+
+			averageAccuracyActive  += rs1.getAccuracy();
+			averageNumTestsActive  += rs1.getNumTests();
+
+			--pNumItter;
+		}
+
+		averageAccuracyActive  /= numItter;
+		averageNumTestsActive  /= numItter;
+
+		System.out.println("Number of Learners Trained: " + numItter );
+		System.out.println("Active Results");
+		System.out.println("Average Accuracy          : " + averageAccuracyActive + "%");
+		System.out.println("Average Number of Tests   : " + averageNumTestsActive      );
+	}
+
+	public static void runPassive(String fileLocation, int classLoc, int pNumItter) throws FileNotFoundException {
+		double averageAccuracyPassive = 0;
+		double averageNumTestsPassive = 0;
+
+		double numItter = pNumItter;
+
+		while(pNumItter > 0) {
+			Dataset data = ARFFHandler.loadARFF(new File(fileLocation), classLoc);
+
+			NormalizeMidrange nmr = new NormalizeMidrange(0, 2);
+			nmr.build(data);
+			nmr.filter(data);
+
+			Sampling s = Sampling.SubSampling;
+
+			Pair<Dataset, Dataset> data2 = s.sample(data, (int) (data.size() * 0.7), 1);
+
+			data = data2.x();
+			Dataset testData = data2.y();
+
+			StopCondition st1 = new StopCondition(data.size());
+			StopCondition st2 = new StopCondition(10, data.size(), .72);
+
+			Passive p = new Passive(data, 1);
+
+			Classifier knn = new KNearestNeighbors(5);
+
+			Learner l2 = new Learner(p , knn, testData, st2);
+
+			l2.run();
+			ResultSet rs2 = l2.getResultSet();
+
+//			System.out.println("Completed test #" + (numItter - pNumItter));
+//			System.out.println("Passive Results");
+//			System.out.println("Accuracy: " + rs2.getAccuracy());
+//			System.out.println("NumTests: " + rs2.getNumTests());
+
+			averageAccuracyPassive += rs2.getAccuracy();
+			averageNumTestsPassive += rs2.getNumTests();
+
+			--pNumItter;
+		}
+		averageAccuracyPassive /= numItter;
+		averageNumTestsPassive /= numItter;
+
+		System.out.println("Number of Learners Trained: " + numItter );
+		System.out.println("Passive Results");
+		System.out.println("Average Accuracy          : " + averageAccuracyPassive + "%");
+		System.out.println("Average Number of Tests   : " + averageNumTestsPassive      );
+
+	}
+
+	public static void runBoth   (String fileLocation, int classLoc, int pNumItter) throws FileNotFoundException {
+		double averageAccuracyActive  = 0;
+		double averageNumTestsActive  = 0;
+		double averageAccuracyPassive = 0;
+		double averageNumTestsPassive = 0;
+
+		double numItter = pNumItter;
+
+		while(pNumItter > 0) {
+			Dataset data = ARFFHandler.loadARFF(new File(fileLocation), classLoc);
 
 			NormalizeMidrange nmr = new NormalizeMidrange(0, 2);
 			nmr.build(data);
@@ -40,26 +168,52 @@ public class LearnerTest {
 			Dataset testData = data2.y();
 
 			StopCondition st1 = new StopCondition(data.size());
-			StopCondition st2 = new StopCondition(10, data.size(), .74);
+			StopCondition st2 = new StopCondition(10, data.size(), .72);
 
 			Passive p = new Passive(data, 1);
 			Center c = new Center(data);
+			ReverseCenter rc = new ReverseCenter(data);
 
 
 			Classifier knn = new KNearestNeighbors(5);
 
-			Learner l = new Learner(c, knn, testData, st2);
-			l.verboseRun();
-//			l.printResultBasic();
-//			l.printResult();
+			Learner l1 = new Learner(rc, knn, testData, st2);
+			Learner l2 = new Learner(p , knn, testData, st2);
 
+			l1.run();
+			l2.run();
+			ResultSet rs1 = l1.getResultSet();
+			ResultSet rs2 = l2.getResultSet();
 
-//			simpleTest(knn, data, testData);
+			System.out.println("Completed test #" + (numItter - pNumItter));
+			System.out.println("Active Results");
+			System.out.println("Accuracy: " + rs1.getAccuracy());
+			System.out.println("NumTests: " + rs1.getNumTests());
+			System.out.println("Passive Results");
+			System.out.println("Accuracy: " + rs2.getAccuracy());
+			System.out.println("NumTests: " + rs2.getNumTests());
 
+			averageAccuracyActive  += rs1.getAccuracy();
+			averageNumTestsActive  += rs1.getNumTests();
+			averageAccuracyPassive += rs2.getAccuracy();
+			averageNumTestsPassive += rs2.getNumTests();
 
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
+			--pNumItter;
 		}
+
+		averageAccuracyActive  /= numItter;
+		averageNumTestsActive  /= numItter;
+		averageAccuracyPassive /= numItter;
+		averageNumTestsPassive /= numItter;
+
+		System.out.println("Number of Learners Trained: " + numItter );
+		System.out.println("Active Results");
+		System.out.println("Average Accuracy          : " + averageAccuracyActive + "%");
+		System.out.println("Average Number of Tests   : " + averageNumTestsActive      );
+		System.out.println("Passive Results");
+		System.out.println("Average Accuracy          : " + averageAccuracyPassive + "%");
+		System.out.println("Average Number of Tests   : " + averageNumTestsPassive      );
+
 	}
 
 	public static void simpleTest(Classifier c, Dataset data, Dataset testData) {
