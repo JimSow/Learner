87121b3fdad118feb61740011338f9b683a57305
Jared <jaredwads@gmail.com>
2015-11-02 21:53:47 -0700

    Made oracles actually do what oracles should

--- src/main/java/Oracle/SimpleOracle.java
+++ src/main/java/Oracle/SimpleOracle.java
@@ -1,7 +1,10 @@
 package Oracle;
 
+import net.sf.javaml.classification.Classifier;
+import net.sf.javaml.classification.KNearestNeighbors;
 import net.sf.javaml.core.Dataset;
 import net.sf.javaml.core.Instance;
+import net.sf.javaml.distance.DistanceMeasure;
 import net.sf.javaml.distance.EuclideanDistance;
 
 /**
@@ -11,12 +14,38 @@ public class SimpleOracle extends IOracle {
 
 	private Dataset data;
 
+	private Classifier c;
+
+	private DistanceMeasure dm;
+
 	public SimpleOracle(Dataset pData) {
 		data = pData;
+		c = new KNearestNeighbors(1);
+		c.buildClassifier(data);
+		dm = new EuclideanDistance();
 	}
 
 	@Override
 	public Object getLabel(Instance unlabeled) {
-		return data.kNearest(1, unlabeled, new EuclideanDistance());
+
+		if(data.size() <= 0){
+			return null;
+		}
+
+		double distance = Double.MAX_VALUE;
+		Instance next = null;
+
+		for(Instance item : data) {
+			double temp = dm.measure(unlabeled, item);
+
+			if(temp < distance) {
+				if(temp == 0){
+					return item.classValue();
+				}
+				distance = temp;
+				next = item;
+			}
+		}
+		return next.classValue();
 	}
 }
