7d6aa1d7da1490110e007cf2c16e94e487c5b107
Jared <jaredwads@gmail.com>
2015-11-02 21:50:10 -0700

    Added Query by Committee learner

---
+++ src/main/java/Learner/HypothesisSpaceSearch/StreamLearning/QueryByCommitee.java
@@ -0,0 +1,13 @@
+package Learner.HypothesisSpaceSearch.StreamLearning;
+
+import net.sf.javaml.core.Instance;
+
+/**
+ * Created by jared on 11/2/15.
+ */
+public class QueryByCommitee extends AStreamLearner {
+	@Override
+	protected Boolean runOne(Instance pNewInstance) {
+		return null;
+	}
+}
