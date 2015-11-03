bcec9b46d34c5155fc38305b160c571c21c0d391
Jared <jaredwads@gmail.com>
2015-11-02 21:50:38 -0700

    Moved several functions into upper level interface

---
+++ src/main/java/Learner/ILearner.java
@@ -0,0 +1,21 @@
+package Learner;
+
+import net.sf.javaml.classification.Classifier;
+
+/**
+ * Created by jared on 10/31/15.
+ */
+public interface ILearner {
+
+	Classifier run();
+	Classifier verbouseRun();
+
+	void printResult();
+
+	ResultSet getResults();
+
+	int getNumTests();
+	double getAccuracy();
+	double getRecall();
+
+}
