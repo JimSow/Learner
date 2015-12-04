4ef3fcee68e34c6f87e5d659abc6b19730af5259
Jared <jaredwads@gmail.com>
2015-12-04 08:12:28 -0700

    Added Exceptions

---
+++ src/main/java/Excptions/SizeNotEqualException.java
@@ -0,0 +1,10 @@
+package Excptions;
+
+/**
+ * Created by jared on 12/2/15.
+ */
+public class SizeNotEqualException extends Exception {
+	public SizeNotEqualException(String message) {
+		super(message);
+	}
+}
