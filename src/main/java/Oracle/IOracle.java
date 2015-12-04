08e14ba2bf7e3cebc3a4f85db8e048d5280ea881
Jared <jaredwads@gmail.com>
2015-12-04 08:16:20 -0700

    Changed to an interface

--- src/main/java/Oracle/IOracle.java
+++ src/main/java/Oracle/IOracle.java
@@ -5,16 +5,6 @@ import net.sf.javaml.core.Instance;
 /**
  * Created by jared on 10/24/15.
  */
-public abstract class IOracle {
-
-	Dataset data;
-
-	public IOracle(Dataset pData) {
-		if(pData == null || pData.size() < 1) {
-			throw new InvalidParameterException("Cant use an empty data set to build the Oracle");
-		}
-		data = pData.copy();
-	}
-
-	public abstract Object getLabel(int index);
+public interface IOracle {
+	Object getLabel(Instance instance);
 }
